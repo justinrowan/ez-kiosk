5dbe9202c0f728c9c502a2f87c12f25e32eea6d0
Daddy <Dashiiie@users.noreply.github.com>
2018-02-15 15:44:33 -0800

    Added basis of the index for testing

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+//Place Holder
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <!-- Meta Stuff -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap 4 CSS -->
+    <link rel="stylesheet" href="css/boostrap.min.css"
+    <title>Page Title</title>
+</head>
+<body>
+
+    <h1>My First Heading</h1>
+    <p>My first paragraph.</p>
+
+</body>
+</html>

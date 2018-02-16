3ea04c21afe8e68aca4acffc64876d8167dcb44d
Daddy <Dashiiie@users.noreply.github.com>
2018-02-15 16:18:35 -0800

    Stylesheets and framework

--- index.php
+++ index.php
@@ -9,14 +9,21 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-    <!-- Bootstrap 4 CSS -->
-    <link rel="stylesheet" href="css/boostrap.min.css"
+    <!-- Bootstrap 4 & CSS -->
+    <link rel="stylesheet" href="/css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href=/css/stylesheet.css">
     <title><?php echo $title ?></title>
 </head>
 <body>
+    <body style="background-color:#3B3A3F;">
 
     <h1>Hello</h1>
     <p>World</p>
 
+
+    <script src="js/jquery-3.2.1.slim.min.js"</script>
+    <script src="js/popper.min.js"</script>
+    <script src="js/bootstrap.min.js"</script>
+  </body>
 </body>
 </html>
